16ac05828e56ba5342243ce98733dbf2cfa0b573
Faizan Eqbal <faizaneqbal1999@gmail.com>
2026-03-27 17:45:27 +0530

    Added GitHub controller with DI and endpoint

---
+++ GitHubConnectorAPI/Controllers/GitHubController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GitHubConnectorAPI.Services;
+
+namespace GitHubConnectorAPI.Controllers
+{
+    // This tells ASP.NET that this is an API controller
+    [ApiController]
+
+    // Base route: api/github
+    [Route("api/github")]
+    public class GitHubController : ControllerBase
+    {
+        // This is our service (business logic layer)
+        private readonly IGitHubService _gitHubService;
+
+        // Constructor Injection (Dependency Injection)
+        public GitHubController(IGitHubService gitHubService)
+        {
+            // .NET automatically gives us the object because of AddScoped
+            _gitHubService = gitHubService;
+        }
+
+        // Endpoint: GET api/github/repos
+        [HttpGet("repos")]
+        public async Task<IActionResult> GetRepositories()
+        {
+            // Calling service (NOT writing logic here)
+            var result = await _gitHubService.GetUserRepositories();
+
+            // Returning response
+            return Ok(result);
+        }
+    }
+}
